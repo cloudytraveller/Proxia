35814091e643ff5373b737e03005f447fd428423
CloudyTraveller <cloudytraveller@riseup.net>
2022-12-30 07:23:35 -0700

    feat: move some utils to Client

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -5,7 +5,8 @@ import { loadCommands, loadEvents, registerSlashCommands } from "../utils/loader
 import { logger } from "../utils/logger.js";
 import { DatabaseManager } from "./Database.js";
 // import { DatabaseManager } from "./Database.js";
-import { Client, Collection, GatewayIntentBits, ActivityType } from "discord.js";
+import { Client, Collection, GatewayIntentBits, ActivityType, Role as DiscordRole } from "discord.js";
+import { randomBytes } from "node:crypto";
 import path from "node:path";
 import url from "node:url";
 
@@ -39,6 +40,33 @@ export class ProxiaClient extends Client {
 
   changeActivityTimer: NodeJS.Timer | undefined;
 
+  readonly utils = {
+    generateUserUniqueId: async (guildId: string): Promise<string> => {
+      let uniqueId;
+      const guildUniqueIds = await this.db.getUniqueIdsForGuild(guildId);
+      while (uniqueId === undefined || guildUniqueIds.includes(uniqueId)) {
+        uniqueId = randomBytes(2).toString("hex");
+      }
+
+      return uniqueId;
+    },
+
+    calculateRoleUniqueIds: async (
+      userRolesCache: Collection<string, DiscordRole>,
+      guildId: string,
+    ): Promise<string[]> => {
+      const guildRoles = await this.db.getRoles(guildId);
+
+      const roleIdMap: Record<string, string> = {};
+
+      for (const role of guildRoles) {
+        roleIdMap[role.id] = role.unique_id;
+      }
+
+      return userRolesCache.map((role) => roleIdMap[role.id]).filter((e) => !!e);
+    },
+  };
+
   constructor(config: ProxiaConfig) {
     super({
       ...config.clientOptions,
@@ -57,8 +85,13 @@ export class ProxiaClient extends Client {
 
   public init() {
     try {
-      // Logs into the Discord API, I guess
-      this.login(this.config.token);
+      const attachmentsDirectory = this.config.attachmentsDirectory;
+      this.config.attachmentsDirectory = path.isAbsolute(attachmentsDirectory)
+        ? attachmentsDirectory
+        : path.resolve(
+            path.join(path.dirname(url.fileURLToPath(import.meta.url)), "../../"),
+            attachmentsDirectory,
+          );
 
       // Wait for the initial login before loading modules
       this.once("ready", async () => {
@@ -71,19 +104,33 @@ export class ProxiaClient extends Client {
         registerSlashCommands(this);
 
         logger.info(
-          `Logged in as ${this.user?.tag} in ${this.guilds.cache.size} guilds ${this.shard && `on shard #${this.shard?.ids[0]}`}`,
+          `Logged in as ${this.user?.tag} in ${this.guilds.cache.size} guilds ${
+            this.shard && `on shard #${this.shard?.ids[0]}`
+          }`,
         );
-        logger.info(`${this.commands.size} commands loaded ${(this.shard && `on shard #${this.shard?.ids[0]}`) || ""}`);
-        logger.info(`${this.events.size} events loaded ${this.shard && `on shard #${this.shard?.ids[0]}`}` || "");
+        logger.info(
+          `${this.commands.size} commands loaded ${
+            (this.shard && `on shard #${this.shard?.ids[0]}`) || ""
+          }`,
+        );
+        logger.info(
+          `${this.events.size} events loaded ${this.shard && `on shard #${this.shard?.ids[0]}`}` ||
+            "",
+        );
 
         // Activity changing
         if (this.config.activities && this.config.activities.length > 0) {
           this.changeActivityTimer = setInterval(
             this.changeActivity,
-            this.config.activityInterval >= 1 ? 1000 * 60 * this.config.activityInterval : 1000 * 60 * 1,
+            this.config.activityInterval >= 1
+              ? 1000 * 60 * this.config.activityInterval
+              : 1000 * 60 * 1,
           );
         }
       });
+
+      // Logs into the Discord API, I guess
+      this.login(this.config.token);
     } catch (error) {
       logger.error(`An error occured while initializing Proxia: ${error}`);
     }
